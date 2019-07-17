46327aa9771bda21d61243844fd5c213bd85a702
noura <noura.aldubaikhai@hotmail.com>
2019-07-17 20:14:44 +0300

    Submit Relaxr Homework

---
+++ starter-code/my-script.js
@@ -0,0 +1,15 @@
+(function () {
+ 
+    Var dialog = getElementById( ‘popup’ );
+     
+    document.getElementById( ‘show’ ) .onclick = function () {
+     
+    dialog.show ();
+     
+    document.getElementById(‘close’) .onclick = function () {
+     
+    dialog.close ();
+     
+    };
+     
+    })();
